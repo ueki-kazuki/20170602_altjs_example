df443769368ada08e5de26dd15c1782e39f2157c
植木和樹 <ueki.kazuki@classmethod.jp>
2017-06-05 08:39:25 +0900

    gulp-autoprefixer and gulp-minify were completed.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var minify = require("gulp-minify");
 
 gulp.task('hello', function() {
   console.log('Hello gulp!');
@@ -8,18 +10,11 @@ gulp.task('hello', function() {
 gulp.task('sass', function() {
   var stream = gulp.src('sass/*.scss')
     .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(minify())
     .pipe(gulp.dest('css'));
   return stream;
 });
 
-//gulp.task('sass', function() {
-//  var stream = gulp.src('sass/*.scss')
-//    .pipe(sass())
-//    .pipe(autoprefixer())
-//    .pipe(minify())
-//    .pipe(gulp.dest('css'));
-//  return stream;
-//});
-
-//gulp.task('default', ['hello','sass']);
+gulp.task('default', ['hello','sass']);
 
