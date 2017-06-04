81874ce8a1c5cc02de50f5c394b62123b1ffa013
植木和樹 <ueki.kazuki@classmethod.jp>
2017-06-05 08:34:47 +0900

    Successfully done for gulp-sass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
- 
+var sass = require("gulp-sass");
+
 gulp.task('hello', function() {
   console.log('Hello gulp!');
 });
@@ -7,11 +8,18 @@ gulp.task('hello', function() {
 gulp.task('sass', function() {
   var stream = gulp.src('sass/*.scss')
     .pipe(sass())
-    .pipe(autoprefixer())
-    .pipe(minify())
     .pipe(gulp.dest('css'));
   return stream;
 });
 
-gulp.task('default', ['hello','sass']);
+//gulp.task('sass', function() {
+//  var stream = gulp.src('sass/*.scss')
+//    .pipe(sass())
+//    .pipe(autoprefixer())
+//    .pipe(minify())
+//    .pipe(gulp.dest('css'));
+//  return stream;
+//});
+
+//gulp.task('default', ['hello','sass']);
 
