49e4bee750ec73bfce57fa230c9aa71719116fd0
植木和樹 <ueki.kazuki@classmethod.jp>
2017-06-05 08:59:32 +0900

    Install gulp-typescript plugin

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,18 @@ var gulp = require('gulp');
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
 var minify = require("gulp-minify");
+var ts = require("gulp-typescript");
+var uglify = require("gulp-uglify");
+ 
+gulp.task("js", function() {
+    gulp.src(["ts/*.ts"])
+        .pipe(ts({
+            noImplicitAny: true,
+            outFile: 'index.js'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest("js"));
+});
 
 gulp.task('sass', function() {
   var stream = gulp.src('sass/*.scss')
@@ -12,5 +24,5 @@ gulp.task('sass', function() {
   return stream;
 });
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass','js']);
 
